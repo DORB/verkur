2373e5cecbca2ebe6b9aa995d1644f442d04c7eb
olikari <olafurks10@ru.is>
2014-12-09 15:46:25 +0000

    Ymsar UI breytingar

--- consoleui.cpp
+++ consoleui.cpp
@@ -679,7 +679,10 @@ void ConsoleUI::rel()
             if(params[2] == "y")
             {
                 r = service.get_rel(m);
-                show(r, m);
+                if(r.size() > 0)
+                    show(r, m);
+                else
+                    cout << "\nThere are no relations connected to this Programmer" << endl;
             }
             else if(params[2] == "n" || params[2] == "N")
             {
@@ -714,7 +717,10 @@ void ConsoleUI::rel()
                 else
                     m.ID = c[atoi(params[2].c_str()) - 1].getID();
                 r = service.get_rel(m);
-                show(r, m);
+                if(r.size() > 0)
+                    show(r, m);
+                else
+                    cout << "\nThere are no relations connected to this Computer" << endl;
             }
         }while(!isOK && params[2] != "q");
     }
@@ -1054,6 +1060,9 @@ void ConsoleUI::marry()
 
     params.clear();
 
+    cout << "\nThank you for choosing marry. The marry function links" << endl;
+    cout << "Programmers and Computers together and saves the relation to database." << endl;
+
     do
     {
         while(!search_successful && !quit)
@@ -1061,7 +1070,7 @@ void ConsoleUI::marry()
             params.clear();
             p.clear();
             cout << "\nSearch for a Programmer to marry." << endl;
-            cout << "Enter search string: ";
+            cout << "Enter search string (press q to back out): ";
             cin >> param;
 
             countParam(params);
@@ -1118,11 +1127,19 @@ void ConsoleUI::marry()
         }
         else if(p.size() > 1)
         {
-            cout << "\nEnter no. of Programmer to marry: ";
-            cin >> param;
-            params.push_back(param);
-            countParam(params);
-            trimParam(params, 2);
+            bool isOK;
+
+            do
+            {
+                cout << "\nEnter no. of Programmer to marry: ";
+                cin >> param;
+
+                params.push_back(param);
+                countParam(params);
+                trimParam(params, 2);
+
+                int temp = isValidInput(param, p.size() + 1, isOK);
+            }while(!isOK);
 
             int temp_id = atoi(params[1].c_str()) - 1;
             show(p[temp_id]);
@@ -1165,7 +1182,8 @@ void ConsoleUI::marry()
             {
                 params.clear();
                 c.clear();
-                cout << "\nSearch for a Computer to marry to your handsome Programmer." << endl;
+                cout << "\nSearch for a Computer to marry your handsome Programmer." << endl;
+                cout << "You can though, back out of the marrage by pressing q" << endl;
                 cout << "Enter search string: ";
                 cin >> param;
 
@@ -1223,7 +1241,7 @@ void ConsoleUI::marry()
             }
             else if(c.size() > 1)
             {
-                cout << "\nEnter no. of Programmer to marry: ";
+                cout << "\nEnter no. of Computer to marry: ";
                 cin >> param;
                 params.push_back(param);
                 countParam(params);
@@ -1232,7 +1250,7 @@ void ConsoleUI::marry()
                 int temp_id = atoi(params[1].c_str()) - 1;
                 show(c[temp_id]);
 
-                cout << "Is this the right Programmer? (y/n) ";
+                cout << "Is this the right Computer? (y/n) ";
                 cin >> param;
                 params.push_back(param);
                 countParam(params);
@@ -1370,7 +1388,7 @@ void ConsoleUI::show(const RelContainer& r, const marriage& m)
         if(m.isPerson)
             cout << r[0].getPname() << " is related to the following computers:" << endl;
         else
-            cout << "The " << r[0].getCname() << " is connected to the following people:" << endl;
+            cout << setw(6) << "The " << r[0].getCname() << " is connected to the following people:" << endl;
 
         cout << "+----------------------------------------------------------------+" << endl;
         for(unsigned int i = 0; i < r.size(); i++)
